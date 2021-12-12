44011afd48a6b7b9ab0c7eed59d6434c434db6db
Emre Celikten <emre@celikten.name>
2021-12-13 01:25:49 +0300

    Add template for days

---
+++ template/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_DATA: &'static str = r"";
+
+    #[test]
+    fn test() {
+
+    }
+
+}
