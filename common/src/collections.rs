0026d62730d6ca46419752bc1257016861f677c0
Emre Celikten <emre@celikten.name>
2021-12-13 16:15:46 +0300

    Day 09

--- common/src/collections.rs
+++ common/src/collections.rs
@@ -1,4 +1,8 @@
-use std::collections::HashMap;
+use crate::error::CommonError;
+use std::collections::{HashMap, VecDeque};
+use std::str::FromStr;
+
+pub const NEIGHBOURS_CROSS_2D: [(i32, i32); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
 pub trait SparseGrid<V: Default> {
     fn get_pos(&self, x: usize, y: usize) -> Option<&V>;
@@ -11,6 +15,8 @@ pub trait DenseGrid<V: Default> {
     fn get_pos(&self, x: usize, y: usize) -> &V;
     fn get_pos_mut(&mut self, x: usize, y: usize) -> &mut V;
     fn set_pos(&mut self, x: usize, y: usize, value: V);
+    fn get_neighbours_cross(&self, x: usize, y: usize) -> Vec<(usize, usize)>;
+    fn boundary_fill_cross(&self, x: usize, y: usize, fill: V, boundary: V) -> (usize, Self);
 }
 
 pub type HashGrid<V> = HashMap<(usize, usize), V>;
@@ -33,10 +39,11 @@ impl<V: Default> SparseGrid<V> for HashGrid<V> {
     }
 }
 
+#[derive(Clone, PartialEq, Debug)]
 pub struct ArrayGrid<V> {
-    x_size: usize,
-    y_size: usize,
-    underlying: Box<[V]>,
+    pub x_size: usize,
+    pub y_size: usize,
+    pub underlying: Box<[V]>,
 }
 
 impl<V: Default + Clone> ArrayGrid<V> {
@@ -49,7 +56,7 @@ impl<V: Default + Clone> ArrayGrid<V> {
     }
 }
 
-impl<V: Default> DenseGrid<V> for ArrayGrid<V> {
+impl<V: Default + Clone + PartialEq> DenseGrid<V> for ArrayGrid<V> {
     fn get_pos(&self, x: usize, y: usize) -> &V {
         &self.underlying[y * self.x_size + x]
     }
@@ -60,6 +67,85 @@ impl<V: Default> DenseGrid<V> for ArrayGrid<V> {
 
     fn set_pos(&mut self, x: usize, y: usize, value: V) {
         self.underlying[y * self.x_size + x] = value;
+    }
+
+    // This should normally return a impl Iterator, but doing it in trait methods seems to be
+    // unstable unfortunately.
+    fn get_neighbours_cross(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let neighbours: Vec<(usize, usize)> = NEIGHBOURS_CROSS_2D
+            .iter()
+            .filter_map(|(x_neigh, y_neigh)| {
+                let x_new = x as i32 + x_neigh;
+                let y_new = y as i32 + y_neigh;
+                if x_new >= 0
+                    && x_new < self.x_size as i32
+                    && y_new >= 0
+                    && y_new < self.y_size as i32
+                {
+                    Some((x_new as usize, y_new as usize))
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        neighbours
+    }
+
+    fn boundary_fill_cross(&self, x: usize, y: usize, fill: V, boundary: V) -> (usize, Self) {
+        let mut new_grid: ArrayGrid<V> = self.clone();
+        let mut num_filled = 0;
+
+        let mut queue = VecDeque::new();
+        queue.push_back((x, y));
+
+        while !queue.is_empty() {
+            let (x_cur, y_cur) = queue.pop_front().unwrap();
+
+            let cur = new_grid.get_pos(x_cur, y_cur);
+            if cur == &boundary || cur == &fill {
+                continue;
+            }
+
+            new_grid.set_pos(x_cur, y_cur, fill.clone());
+            num_filled += 1;
+
+            let neighbours = self.get_neighbours_cross(x_cur, y_cur);
+            for (x_neigh, y_neigh) in neighbours {
+                let neigh = new_grid.get_pos(x_neigh, y_neigh);
+                if neigh != &boundary && neigh != &fill {
+                    queue.push_back((x_neigh, y_neigh));
+                }
+            }
+        }
+
+        (num_filled, new_grid)
+    }
+}
+
+impl FromStr for ArrayGrid<char> {
+    type Err = CommonError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let result: Vec<char> = s.chars().filter(|ch| *ch != '\n').collect();
+
+        let x_size = s
+            .chars()
+            .position(|ch| ch == '\n')
+            .ok_or(CommonError::Parse(
+                "No linebreaks found while converting to grid.",
+            ))?;
+        let y_size = result.len() / x_size;
+
+        if y_size * x_size != result.len() {
+            Err(CommonError::Parse("Grid data is malformed."))
+        } else {
+            Ok(Self {
+                x_size,
+                y_size,
+                underlying: result.into_boxed_slice(),
+            })
+        }
     }
 }
 
@@ -117,5 +203,53 @@ mod tests {
     fn test_arraygrid_get_set() {
         let mut array_grid = ArrayGrid::new(5, 5);
         test_dense_get_set(&mut array_grid);
+    }
+
+    #[test]
+    fn test_arraygrid_parse() {
+        let data = r"1234567
+2345678";
+
+        let array_grid = ArrayGrid::from_str(data).unwrap();
+        assert_eq!(array_grid.x_size, 7);
+        assert_eq!(array_grid.y_size, 2);
+        assert_eq!(*array_grid.get_pos(2, 1), '4');
+        assert_eq!(*array_grid.get_pos(6, 0), '7');
+        assert_eq!(*array_grid.get_pos(6, 1), '8');
+    }
+
+    #[test]
+    fn test_boundary_fill_cross() {
+        let data = r"#######
+#.....#
+#...#.#
+##.#.##";
+
+        let expected = r"#######
+#XXXXX#
+#XXX#X#
+##X#.##";
+        let array_grid = ArrayGrid::from_str(data).unwrap();
+        let expected_grid = ArrayGrid::from_str(expected).unwrap();
+
+        for x in 1..=5 {
+            let (num_filled, filled_grid) = array_grid.boundary_fill_cross(x, 1, 'X', '#');
+            assert_eq!(num_filled, 10);
+            assert_eq!(filled_grid, expected_grid);
+        }
+
+        for x in 1..=3 {
+            let (num_filled, filled_grid) = array_grid.boundary_fill_cross(x, 2, 'X', '#');
+            assert_eq!(num_filled, 10);
+            assert_eq!(filled_grid, expected_grid);
+        }
+
+        let (num_filled, filled_grid) = array_grid.boundary_fill_cross(5, 2, 'X', '#');
+        assert_eq!(num_filled, 10);
+        assert_eq!(filled_grid, expected_grid);
+
+        let (num_filled, filled_grid) = array_grid.boundary_fill_cross(2, 3, 'X', '#');
+        assert_eq!(num_filled, 10);
+        assert_eq!(filled_grid, expected_grid);
     }
 }
---
+++ d09/src/main.rs
@@ -0,0 +1,93 @@
+use common::collections::{ArrayGrid, DenseGrid};
+use common::error::CommonError;
+use std::fs::read_to_string;
+use std::str::FromStr;
+
+type CoordPair = (usize, usize);
+
+fn find_minima_positions(grid: &ArrayGrid<char>) -> Vec<CoordPair> {
+    let mut minima = Vec::new();
+    for y in 0..grid.y_size {
+        for x in 0..grid.x_size {
+            let cur = grid.get_pos(x, y);
+            let is_minimum = grid
+                .get_neighbours_cross(x, y)
+                .iter()
+                .all(|&(x_n, y_n)| grid.get_pos(x_n, y_n) > cur);
+
+            if is_minimum {
+                minima.push((x, y))
+            }
+        }
+    }
+    minima
+}
+
+fn calculate_risk_level_sum(grid: &ArrayGrid<char>, minima_positions: &[CoordPair]) -> usize {
+    minima_positions
+        .iter()
+        .map(|&(x, y)| grid.get_pos(x, y).to_digit(10).unwrap() as usize + 1)
+        .sum()
+}
+
+fn calculate_basin_size_mul(grid: &ArrayGrid<char>, minima_positions: &[CoordPair]) -> usize {
+    let mut cur_grid = grid.clone();
+    let mut basin_size: Vec<usize> = Vec::new();
+    for &(x, y) in minima_positions {
+        let (num_filled, new_grid) = cur_grid.boundary_fill_cross(x, y, '*', '9');
+        cur_grid = new_grid;
+        if num_filled > 0 {
+            basin_size.push(num_filled);
+        }
+    }
+
+    basin_size.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    basin_size.iter().take(3).product()
+}
+
+fn main() -> Result<(), CommonError> {
+    let grid = ArrayGrid::from_str(&read_to_string("input")?)?;
+    let minima_positions = find_minima_positions(&grid);
+
+    let risk_level_sum = calculate_risk_level_sum(&grid, &minima_positions);
+    println!("Result #1: {}", risk_level_sum);
+
+    let basin_size_mul = calculate_basin_size_mul(&grid, &minima_positions);
+    println!("Result #2: {}", basin_size_mul);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_DATA: &'static str = r"2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+
+    #[test]
+    fn test_calculate_minima_sum() {
+        let grid = ArrayGrid::from_str(&TEST_DATA).unwrap();
+        let minima_positions = find_minima_positions(&grid);
+
+        assert_eq!(minima_positions.len(), 4);
+        assert_eq!(minima_positions[0], (1, 0));
+        assert_eq!(minima_positions[1], (9, 0));
+        assert_eq!(minima_positions[2], (2, 2));
+        assert_eq!(minima_positions[3], (6, 4));
+
+        let sum = calculate_risk_level_sum(&grid, &minima_positions);
+        assert_eq!(sum, 15);
+    }
+
+    #[test]
+    fn test_calculate_basin_size_mul() {
+        let grid = ArrayGrid::from_str(&TEST_DATA).unwrap();
+        let minima_positions = find_minima_positions(&grid);
+        let sum = calculate_basin_size_mul(&grid, &minima_positions);
+        assert_eq!(sum, 1134);
+    }
+}
